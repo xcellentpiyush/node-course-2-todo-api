f2f57f6623a3ad0bd681e72d868c3a8da675c765
Piyush Jain <xcellent.piyush@gmail.com>
2018-10-22 00:00:06 +0300

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -24,6 +25,23 @@ app.get('/todos', (request, response) => {
     Todo.find().then((todos) => {
         response.send({ todos });
     }, (error) => {
+        response.status(400).send(error);
+    });
+});
+
+app.get('/todos/:id', (request, response) => {
+    var id = request.params.id;
+    if (!ObjectID.isValid(id)) {
+        return response.status(404).send('Invalid Id');
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
+            return response.status(404).send();
+        }
+
+        response.send({ todo });
+    }).catch((error) => {
         response.status(400).send(error);
     });
 });
