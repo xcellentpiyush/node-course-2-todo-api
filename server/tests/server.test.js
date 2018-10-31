a652feedcd77f416a275ddddf67aa5a6b96bc9cc
Piyush Jain <xcellent.piyush@gmail.com>
2018-10-31 23:02:31 +0300

    Added test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const { ObjectID } = require('mongodb');
 
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 var todos = [{
+    _id: new ObjectID(),
     text: 'Text1 Added'
 }, {
+    _id: new ObjectID(),
     text: 'Text2 Added'
 }];
 
@@ -72,6 +75,33 @@ describe('GET /todos', () => {
             .expect((response) => {
                 expect(response.body.todos.length).toBe(2);
             })
+            .end(done);
+    });
+});
+
+describe('GET /todos/:id', () => {
+    it('should get the requested doc', (done) => {
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((response) => {
+                expect(response.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        const id = new ObjectID();
+        request(app)
+            .get(`/todos/${id.toHexString()}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return 404 for non-object ids', (done) => {
+        request(app)
+            .get('/todos/piyush')
+            .expect(404)
             .end(done);
     });
 });
